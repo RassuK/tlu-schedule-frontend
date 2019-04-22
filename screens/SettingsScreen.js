d974a963baba7ed3c4d0d11868ace75b9d4d2bad
RassuK <RassuK@users.noreply.github.com>
2019-04-22 15:58:04 +0300

    cleanup

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -4,46 +4,14 @@ import { AppRegistry, Text, TextInput, View, ScrollView, StyleSheet } from 'reac
 import axios from 'axios';
 
 export default class Settings extends Component {
-    /*constructor(props) {
-      super(props);
-      persons: [];
-    }*/
-
-    state={
-        persons: []
-    }
-
-    componentDidMount() {
-        axios.get(`https://jsonplaceholder.typicode.com/users`)
-            .then(res => {
-                const persons = res.data;
-                this.setState({ persons });
-            })
-    }
 
     render() {
         return (
-            <View style={styles.container}>
-                <ScrollView style={styles.container}>
-                    { this.state.persons.map(person =>
-                        <Text>
-                            settings
-                        </Text>)}
-                </ScrollView>
+            <View>
+                <Text>
+                    Settings
+                </Text>
             </View>
         )
     }
 }
-
-const styles = StyleSheet.create({
-    bigBlue: {
-        color: 'blue',
-        fontWeight: 'bold',
-        fontSize: 30,
-    },
-    container: {
-        flex:1,
-        backgroundColor: 'white',
-        marginTop: 10
-    },
-});
