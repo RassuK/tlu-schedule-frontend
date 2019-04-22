c3909bf07c90d5d2bdc0aac9cce731ce838c0b23
RassuK <RassuK@users.noreply.github.com>
2019-04-22 15:56:30 +0300

    android fix

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, TextInput, View, ScrollView, StyleSheet } from 'react-native';
-import { ListItem } from 'react-native-elements'
+
+import ScheduleItem from '../components/ScheduleItem'
 
 import axios from 'axios';
 
 const schedule = [
-    {lectureName : "Asjade Internet", startTime : "10:15", endTime : "13:45", lecturerName : "Jaagup Kippar", classRoom : "A-512"},
-    {lectureName : "Teoreetiline Suitsiid", startTime : "10:15", endTime : "13:45", lecturerName : "Demon Man", classRoom : "A-512"},
-    {lectureName : "Mobiilirakenduste arendamine", startTime : "10:15", endTime : "13:45", lecturerName : "Teacher Boy", classRoom : "A-512"},
+    {lectureName : "Asjad Internet", startTime : "10:15", endTime : "13:45", lecturerName : "Jaagup Kippar", classRoom : "A-512"},
+    {lectureName : "Teoreetiline Informaatika", startTime : "10:15", endTime : "13:45", lecturerName : "Peeter Normak", classRoom : "A-512"},
+    {lectureName : "Mobiilirakenduste arendamine", startTime : "10:15", endTime : "13:45", lecturerName : "Gorislavski Roman", classRoom : "A-512"},
 ]
 
 export default class Home extends Component {
+    /*
+    ###API call example
+
     state={
         persons:[]
     }
-
-
 
     componentDidMount() {
         axios.get(`https://jsonplaceholder.typicode.com/users`)
@@ -24,34 +26,25 @@ export default class Home extends Component {
                 this.setState({ persons });
             })
     }
+    */
 
     render() {
         return (
-            <ScrollView>
-                {
-                    schedule.map((l, i) => (
-                        <ListItem
-                            key={i}
-                            title={l.lectureName}
-                            subtitle={l.lecturerName + " / " + l.classRoom}
-                            rightElement={l.startTime + "-" + l.endTime}
-                        />
-                    ))
-                }
-            </ScrollView>
+
+                    <ScrollView>
+                        {
+                            schedule.map((l, i) => (
+                                <ScheduleItem
+                                    key = {i}
+                                    lectureName = {l.lectureName}
+                                    lecturerName = {l.lecturerName}
+                                    classRoom = {l.classRoom}
+                                    startTime = {l.startTime}
+                                    endTime = {l.endTime}
+                                />
+                            ))
+                        }
+                    </ScrollView>
         )
     }
 }
-
-const styles = StyleSheet.create({
-    bigBlue: {
-        color: 'blue',
-        fontWeight: 'bold',
-        fontSize: 30,
-    },
-    container: {
-        flex:1,
-        backgroundColor: 'white',
-        marginTop: 10
-    },
-});
